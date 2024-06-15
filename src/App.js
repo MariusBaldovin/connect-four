f241d2781e77fb6bfb9efb126e956cfbf543ae8f
MariusBaldovin <youremailid@example.com>
2024-06-15 08:37:09 +0100

    created the board based on number of cols and rows from the user input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Home from "./components/Home";
+import Board from "./components/Board";
 
 const App = () => {
-  const startGame = useCallback((rows, cols, winLength) => {
+  const [rows, setRows] = useState(6);
+  const [cols, setCols] = useState(7);
+  const [winLength, setWinLength] = useState(4);
+  const [currentPlayer, setCurrentPlayer] = useState("Red");
+  const [grid, setGrid] = useState(
+    Array(6)
+      .fill()
+      .map(() => Array(7).fill(" "))
+  );
+  const [hoverRow, setHoverRow] = useState(null); // New state to track hover row
+  const [hoverCol, setHoverCol] = useState(null); // New state to track hover column
+  const [winner, setWinner] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  // Callback function to start the game
+  const startGame = useCallback((newRows, newCols, newWinLength) => {
     console.log(
       `Starting game with rows: ${rows}, cols: ${cols}, winLength: ${winLength}`
     );
+    setRows(newRows);
+    setCols(newCols);
+    setWinLength(newWinLength);
+    setGrid(
+      Array(newRows)
+        .fill()
+        .map(() => Array(newCols).fill(" "))
+    );
+    setCurrentPlayer("Red");
+    setWinner(null);
+    setHoverRow(null);
+    setHoverCol(null);
+    setGameStarted(true);
   }, []);
 
   return (
     <>
-      <div className=" mt-10 text-4xl text-center font-extrabold text-slate-900">
+      <div className="mt-10 text-4xl text-center font-extrabold text-slate-900">
         CONNECT FOUR GAME
       </div>
-      <Home startGame={startGame} />
+      {gameStarted ? (
+        <>
+          <Board
+            grid={grid}
+            hoverRow={hoverRow}
+            hoverCol={hoverCol}
+            setHoverRow={setHoverRow}
+            setHoverCol={setHoverCol}
+            //dropToken={dropToken}
+            currentPlayer={currentPlayer}
+          />
+        </>
+      ) : (
+        <Home startGame={startGame} />
+      )}
     </>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Cell from "./Cell";
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ grid, setHoverRow, setHoverCol }) => {
+  return (
+    <div
+      className="board grid gap-1 justify-center"
+      onMouseLeave={() => {
+        setHoverRow(null);
+        setHoverCol(null);
+      }}
+    >
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row flex gap-1">
+          {row.map((cell, colIndex) => (
+            <Cell key={colIndex} value={cell} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Board;
